c71c4efed1bd0155c5679086d201a4b43469f114
Katta Pratheek Reddy (RBEI/BSL7) <tkt2kor@bosch.com>
2020-07-04 14:01:44 +0530

    hello

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ const publicDirectoryPath = path.join(__dirname, '/public')
 const viewsPath = path.join(__dirname, '/templates')
 
 
-app.set('view engine', 'hbs')
+app.set('view engine', 'hbs') 
 app.set('views', viewsPath)
 
 app.use(express.static(publicDirectoryPath))
-
+ 
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather App',
